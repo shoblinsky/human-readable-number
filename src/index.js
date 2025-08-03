46bb46431c6f80c5c8f43a243210a21429fed281
shoblinsky <123806946+shoblinsky@users.noreply.github.com>
2025-08-03 21:55:52 +0500

    init: start human readable number task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function toReadable(/* number */) {
+module.exports = function toReadable(number) {
+  const alphabetic = {
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+
+  }
   throw new Error('Not implemented');
 };
