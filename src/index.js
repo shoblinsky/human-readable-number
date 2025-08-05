140576a8db968564ec6fa6b01d2a1e59416bb32b
shoblinsky <123806946+shoblinsky@users.noreply.github.com>
2025-08-05 17:02:01 +0500

    feat: made task

--- src/index.js
+++ src/index.js
@@ -1,65 +1,62 @@
 module.exports = function toReadable(number) {
   const alphabetic = {
-    1: "one",
-    2: "two",
-    3: "three",
-    4: "four",
-    5: "five",
-    6: "six",
-    7: "seven",
-    8: "eight",
-    9: "nine",
-    10: "ten",
-    11: "eleven",
-    12: "twelve",
-    13: "thirteen",
-    14: "fourteen",
-    15: "fifteen",
-    16: "sixteen",
-    17: "seventeen",
-    18: "eighteen",
-    19: "nineteen",
-  }
-  // number < 20 ? return : go to tens
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    10: 'ten',
+    11: 'eleven',
+    12: 'twelve',
+    13: 'thirteen',
+    14: 'fourteen',
+    15: 'fifteen',
+    16: 'sixteen',
+    17: 'seventeen',
+    18: 'eighteen',
+    19: 'nineteen',
+  };
 
   const tens = {
-    20: "twenty",
-    30: "thirty",
-    40: "forty",
-    50: "fifty",
-    60: "sixty",
-    70: "seventy",
-    80: "eighty",
-    90: "ninety",
+    20: 'twenty',
+    30: 'thirty',
+    40: 'forty',
+    50: 'fifty',
+    60: 'sixty',
+    70: 'seventy',
+    80: 'eighty',
+    90: 'ninety',
+  };
+
+  const hundred = 'hundred';
+
+  const thousand = 'thousand';
+
+  let result = '';
+
+  if (number >= 1000) {
+    const thousands = Math.floor(number / 1000);
+    result += `${alphabetic[thousands]} ${thousand} `;
+    number %= 1000;
+  }
+  if (number >= 100) {
+    const hundreds = Math.floor(number / 100);
+    result += `${alphabetic[hundreds]} ${hundred} `;
+    number %= 100;
   }
 
-
-  const hundred = {
-    100: "hundred",
+  if (number >= 20) {
+    const ten = Math.floor(number / 10);
+    result += `${tens[ten]} `;
+    number %= 10;
   }
 
-  const thousand = {
-    1000: "thousand",
+  if (number > 0) {
+    result += `${alphabetic[number]} `;
   }
-
-
-  throw new Error('Not implemented');
+  return result;
 };
-
-
-//test
-
-function theHuh(num) {
-  aboveHun = 0;
-  aboveTen = 0;
-  aboveOne = 0;
-  result = '';
-  if (num > 100) {
-    aboveHun = Math.floor(num / 100)
-    aboveTen = num % 100
-    aboveOne = num % 10
-  }
-  console.log(aboveHun)
-  console.log(aboveTen)
-  console.log(aboveOne)
-}
